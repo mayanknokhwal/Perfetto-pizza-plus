121ac6ac871198815061796971cfdee9e92597ef
mayanknokhwal <44website.com44@gmail.com>
2026-08-21 18:51:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-/**
- * Perfetto Pizza - Customer App Script Loader
- * Forwards to app.js for full customer ordering, Leaflet GPS location picker, and checkout flow.
- */
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = require('./app');
-}
